a188d07173a22371ab70c5dd283e903c9e57a50a
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-07-27 17:31:41 -0300

    Pré-logica para ler a foto da pessoa.

--- controllers/leitura.py
+++ controllers/leitura.py
@@ -6,21 +6,31 @@ def index():
 
     form = FORM(T('Matricula'), INPUT(_name='matricula', requires=IS_NOT_EMPTY()), INPUT(_type='submit'))
     desc = None
+    foto = URL("static", "images/silhueta.png")
 
     if form.accepts(request, session):
         response.flash = 'Lido'
 
         matricula = form.vars['matricula']
-        params = {
-            'LMIN': 0,
-            'LMAX': 1000,
-            'MATRICULA': matricula,
-        }
 
         try:
+            params = {
+                'LMIN': 0,
+                'LMAX': 1,
+                'MATRICULA': matricula,
+            }
             result = api.get('V_PESSOAS_DADOS', params)  # type: unirio.api.result.APIRestultObject
             desc = result.content[0]
+
+            # Por enquanto deixa quieto isso até ver como lidar com a foto vinda do banco.
+            # eh_aluno = (desc.descricao_vinculo == 1 or desc.descricao_vinculo == 2)
+            # nome_da_tabela_de_foto = 'V_ALU_FOTO' if eh_aluno else 'V_FUNC_FOTO'
+            # result = api.get(nome_da_tabela_de_foto, params)
+            # if result.content[0]['foto'] is not None:
+            #     foto = result.content[0]['foto']
+
         except:
+            response.flash = 'Erro na consulta ao banco de dados'
             pass
 
     elif form.errors:
@@ -29,4 +39,4 @@ def index():
         response.flash = 'Insira a matricula'
 
 
-    return dict(form=form, desc=desc)
+    return dict(form=form, desc=desc, foto=foto)
