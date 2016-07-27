a9f36b5759c0d50a85b6017b50ea7d0ebcb820db
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-07-27 17:37:29 -0300

    Comentarios

--- controllers/leitura.py
+++ controllers/leitura.py
@@ -5,9 +5,10 @@ __author__ = 'carlosfaruolo'
 def index():
 
     form = FORM(T('Matricula'), INPUT(_name='matricula', requires=IS_NOT_EMPTY()), INPUT(_type='submit'))
-    desc = None
-    foto = URL("static", "images/silhueta.png")
+    descricao = None
+    url_foto = URL("static", "images/silhueta.png")
 
+    # aqui virao coisas do form
     if form.accepts(request, session):
         response.flash = 'Lido'
 
@@ -20,7 +21,7 @@ def index():
                 'MATRICULA': matricula,
             }
             result = api.get('V_PESSOAS_DADOS', params)  # type: unirio.api.result.APIRestultObject
-            desc = result.content[0]
+            descricao = result.content[0]
 
             # Por enquanto deixa quieto isso até ver como lidar com a foto vinda do banco.
             # eh_aluno = (desc.descricao_vinculo == 1 or desc.descricao_vinculo == 2)
@@ -33,10 +34,13 @@ def index():
             response.flash = 'Erro na consulta ao banco de dados'
             pass
 
+    # aqui caso ocorreu xabu
     elif form.errors:
         response.flash = 'Erro na leitura'
+
+    # mensagem ao entrar. podemos tirar isso tambem
     else:
         response.flash = 'Insira a matricula'
 
 
-    return dict(form=form, desc=desc, foto=foto)
+    return dict(form=form, desc=descricao, foto=url_foto)
