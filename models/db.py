9f2f52f7a8761545f1927d6e35a78ee9f1391481
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-07-27 15:57:50 -0300

    Usando myconf (private/appconfig.ini) como parametro para db

--- models/db.py
+++ models/db.py
@@ -18,7 +18,8 @@ from gluon import current
 
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
+    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int))
+
 else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
     db = DAL('google:datastore+ndb')
@@ -99,10 +100,6 @@ auth.settings.reset_password_requires_verification = True
 ## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
 ## >>> for row in rows: print row.id, row.myfield
 #########################################################################
-
-
-
-db = DAL('postgres://postgres:devdtic2@teste.sistemas.unirio.br/restaurante')
 
 db.define_table('refeicoes',
                 Field('descricao', 'string', length=100, label='Descrição'),
